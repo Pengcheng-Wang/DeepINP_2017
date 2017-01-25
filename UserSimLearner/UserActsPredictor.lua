f950edc35810fe13cb75ffd3dbbb554b35fade35
Pengcheng Wang <pwang8@ncsu.edu>
2017-01-25 00:39:02 -0500

    LSTM predictor added

--- UserSimLearner/UserActsPredictor.lua
+++ UserSimLearner/UserActsPredictor.lua
@@ -306,8 +306,8 @@ function CIUserActsPredictor:trainOneEpoch()
                         local input = self.rnnRealUserDataStates[randInd][j]
                         input = self.ciUserSimulator:preprocessUserStateData(input, opt.prepro)
                         local target = self.rnnRealUserDataActs[randInd][j]
-                        inputs[j][k] = input:clone()
-                        targets[j][k] = target:clone()
+                        inputs[j][k] = input
+                        targets[j][k] = target
                     end
                     k = k + 1
                 end
