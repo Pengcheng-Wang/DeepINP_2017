471a219c170458c118ff020d42087ff4e1cb762c
Pengcheng Wang <pwang8@ncsu.edu>
2017-02-04 12:33:30 -0500

    Modified OneStepQAgent in async module, and checked SarsaAgent, which does not need any change

--- async/OneStepQAgent.lua
+++ async/OneStepQAgent.lua
@@ -11,6 +11,11 @@ function OneStepQAgent:_init(opt, policyNet, targetNet, theta, targetTheta, atom
   self.agentName = 'OneStepQAgent'
   self.lstm = opt.recurrent and self.policyNet:findModules('nn.FastLSTM')[1]
   self.lstmTarget = opt.recurrent and self.targetNet:findModules('nn.FastLSTM')[1]
+
+  self.opt = opt
+  -- Sorry, adding ugly code here again, just for CI data compatability
+  self.CIActAdpBound = {{1, 3}, {4, 5}, {6, 8}, {9, 10}}
+
   classic.strict(self)
 end
 
@@ -73,10 +78,43 @@ function OneStepQAgent:accumulateGradient(state, action, state_, reward, termina
   end
   if not terminal then
       local QPrimes = self.targetNet:forward(state_):squeeze()
-      local APrimeMax = QPrimes:max(1):squeeze()
+      local APrimeMax = QPrimes:max(1):squeeze()    -- So, QPrime is a 1-dim tensor with size 10 (acts)
+
+      -- If it is CI data, pick up actions according to adpType
+      local adpT = 0
+      if self.opt.env == 'UserSimLearner/CIUserSimEnv' then   -- Todo: pwang8. Check correctness
+          if state_[-1][1][-4] > 0.1 then adpT = 1 elseif state_[-1][1][-3] > 0.1 then adpT = 2 elseif state_[-1][1][-2] > 0.1 then adpT = 3 elseif state_[-1][1][-1] > 0.1 then adpT = 4 end
+          assert(adpT >=1 and adpT <= 4)
+      end
+
+      if self.opt.env == 'UserSimLearner/CIUserSimEnv' then   -- Todo: pwang8. Check correctness
+          local maxAct = self.CIActAdpBound[adpT][1]
+          local maxActQValue = QPrimes[maxAct]
+          for i=maxAct+1, self.CIActAdpBound[adpT][2] do
+              if QPrimes[i] > maxActQValue then
+                  maxActQValue = QPrimes[i]
+                  maxAct = i
+              end
+          end
+          APrimeMax = maxActQValue
+      end
 
       if self.doubleQ then
-          local _,APrimeMaxInds = self.policyNet:forward(state_):squeeze():max(1)
+          local dqQPrimes = self.policyNet:forward(state_):squeeze()
+          local _,APrimeMaxInds = dqQPrimes:max(1)
+
+          if self.opt.env == 'UserSimLearner/CIUserSimEnv' then   -- Todo: pwang8. Check correctness
+              local maxAct = self.CIActAdpBound[adpT][1]
+              local maxActQValue = dqQPrimes[maxAct]
+              for i=maxAct+1, self.CIActAdpBound[adpT][2] do
+                  if dqQPrimes[i] > maxActQValue then
+                      maxActQValue = dqQPrimes[i]
+                      maxAct = i
+                  end
+              end
+              APrimeMaxInds[1] = maxAct
+          end
+
           APrimeMax = QPrimes[APrimeMaxInds[1]]
       end
 
