b1b9ea5e3ede6d43f2cd44c0713b8c79f9df19b4
Pengcheng Wang <pwang8@ncsu.edu>
2017-05-08 19:02:18 -0400

    Added user simulation model evaluation methods for non-lstm models and shared layer settings

--- UserSimLearner/UserBehaviorGenEvaluator.lua
+++ UserSimLearner/UserBehaviorGenEvaluator.lua
@@ -16,6 +16,17 @@ local TableSet = require 'MyMisc.TableSetMisc'
 local CIUserBehaviorGenEvaluator = classic.class('UserBehaviorGenEvaluator')
 
 function CIUserBehaviorGenEvaluator:_init(CIUserSimulator, CIUserActsPred, CIUserScorePred, CIUserActScorePred, opt)
+
+    local tltCnt = 0
+    local crcActCnt = 0
+    local crcRewCnt = 0
+    local userInd = 1
+    local earlyTotAct = torch.Tensor(opt.lstmHist+81):fill(1e-6)
+    local earlyCrcAct = torch.Tensor(opt.lstmHist+81):fill(0)
+    local firstActDist = torch.Tensor(CIUserSimulator.CIFr.usrActInd_end):fill(0)   -- 15 user actions
+
+    local countScope=0  -- This param is used to calculate action distribution at countScope time step
+
     if opt.sharedLayer < 1 then
 
         -- Bipartitle Act/Score prediction model
@@ -33,23 +44,12 @@ function CIUserBehaviorGenEvaluator:_init(CIUserSimulator, CIUserActsPred, CIUse
         self.opt = opt
 
         if opt.uppModel == 'lstm' then
-
-            local countScope = 0 --4
-            local st = torch.Tensor(CIUserSimulator.CIFr.usrActInd_end):fill(0)     -- tensor dim is 15 (user action types)
-            for k, v in ipairs(CIUserSimulator.realUserDataStartLines) do
-                st[CIUserSimulator.realUserDataActs[v+countScope]] = st[CIUserSimulator.realUserDataActs[v+countScope]] +1 -- check act dist at each x-th time step
-            end
-            print('Act count at time step ', countScope+1, ' is:', st)
+            -- sharedLayer == 0 and lstm model
+--            self._actionDistributionCalc(CIUserSimulator, countScope)
 
             self.userActsPred:forget()
             self.userScorePred:forget()
-            local tltCnt = 0
-            local crcActCnt = 0
-            local crcRewCnt = 0
-            local userInd = 1
-            local earlyTotAct = torch.Tensor(opt.lstmHist+81):fill(1e-6)
-            local earlyCrcAct = torch.Tensor(opt.lstmHist+81):fill(0)
-            local firstActDist = torch.Tensor(15):fill(0)
+
             for i=1, #CIUserActsPred.rnnRealUserDataStates do
                 local userState = CIUserActsPred.rnnRealUserDataStates[i]
                 local userAct = CIUserActsPred.rnnRealUserDataActs[i]
@@ -73,13 +73,14 @@ function CIUserBehaviorGenEvaluator:_init(CIUserSimulator, CIUserActsPred, CIUse
                     if lps[l] == userAct[opt.lstmHist] then
                         crcActCnt = crcActCnt + 1
                         crtExt = true
+                        break
                     end
                 end
 
                 local indDiff = i - CIUserActsPred.rnnRealUserDataStarts[userInd]
                 if indDiff >= 0 and indDiff <= opt.lstmHist+80 and i <= CIUserActsPred.rnnRealUserDataEnds[userInd] then
-                    if crtExt then  --ain[1] == userAct[opt.lstmHist] then
-                    earlyCrcAct[indDiff+1] = earlyCrcAct[indDiff+1] + 1 -- This is at the indDiff's time step, what is prob of correct act prediction
+                    if crtExt then
+                        earlyCrcAct[indDiff+1] = earlyCrcAct[indDiff+1] + 1 -- This is at the indDiff's time step, what is prob of correct act prediction
                     end
                     earlyTotAct[indDiff+1] = earlyTotAct[indDiff+1] + 1
                 end
@@ -93,86 +94,207 @@ function CIUserBehaviorGenEvaluator:_init(CIUserSimulator, CIUserActsPred, CIUse
 
                 tltCnt = tltCnt + 1
 
-                --            if userAct[opt.lstmHist] == CIUserSimulator.CIFr.usrActInd_end then
                 self.userActsPred:forget()
                 self.userScorePred:forget()
-                --            end
 
             end
 
-            print('1st act: ', firstActDist)
+--            print('1st act: ', firstActDist)
             print('Following stats are act pred accuracy, reward pred accuracy, act pred accu of each time step')
             print('###', crcActCnt/tltCnt, crcRewCnt/#CIUserActsPred.rnnRealUserDataEnds, torch.cdiv(earlyCrcAct, earlyTotAct))
 
         else
-            --        print('This part of the evaluation code does not have to be correct. I have not carefully checked it.')
-            --        local userStates = torch.Tensor(CIUserSimulator.userStateFeatureCnt):fill(0)
-            --        userStates[CIUserSimulator.CIFr.userStateGamePlayFeatureCnt + 1] = 0
-            --        userStates[CIUserSimulator.CIFr.userStateGamePlayFeatureCnt + 2] = 5
-            --        userStates[CIUserSimulator.CIFr.userStateGamePlayFeatureCnt + 3] = 9
-            --
-            --        local prepStates = CIUserSimulator:preprocessUserStateData(userStates, opt.prepro)
-            --        local inPrepStates = torch.Tensor(1, CIUserSimulator.userStateFeatureCnt)
-            --        inPrepStates[1] = prepStates:clone()
-            --
-            --        local nll_acts = self.userActsPred:forward(inPrepStates)
-            --        lp, ain = torch.max(nll_acts[1]:squeeze(), 1)
-            --        print('##', lp, ain, nll_acts[1]:squeeze())
-            --
-            --        local st = torch.Tensor(15):fill(0)
-            --        for k, v in ipairs(CIUserSimulator.realUserDataStartLines) do
-            --            st[CIUserSimulator.realUserDataActs[v]] = st[CIUserSimulator.realUserDataActs[v]] +1
-            --        end
-            --        print('Act count\n', st)
-            --
-            --        self.userActsPred:forget()
-            ----        self.userScorePred:forget()
-            --        local tltCnt = 0
-            --        local crcActCnt = 0
-            --        local crcRewCnt = 0
-            --        local userInd = 1
-            --        local earlyTotAct = 0
-            --        local earlyCrcAct = 0
-            --        for i=1, #CIUserSimulator.realUserDataStates do
-            --            local userState = CIUserSimulator:preprocessUserStateData(CIUserSimulator.realUserDataStates[i], opt.prepro)
-            --            local userAct = CIUserSimulator.realUserDataActs[i]
-            --            local userRew = CIUserSimulator.realUserDataRewards[i]
-            --
-            --            local prepUserState = torch.Tensor(1, CIUserSimulator.userStateFeatureCnt)
-            --            prepUserState[1] = userState:clone()
-            --
-            --            local nll_acts = self.userActsPred:forward(prepUserState)
-            --            lp, ain = torch.max(nll_acts[1]:squeeze(), 1)
-            --            if ain[1] == userAct then crcActCnt = crcActCnt + 1 end
-            --
-            --            if i == CIUserSimulator.realUserDataEndLines[userInd] then
-            --                userInd = userInd+1
-            --            elseif i == CIUserSimulator.realUserDataStartLines[userInd] and i <= CIUserSimulator.realUserDataEndLines[userInd] then
-            --                if ain[1] == userAct then
-            --                    earlyCrcAct = earlyCrcAct + 1
-            --                end
-            --                earlyTotAct = earlyTotAct + 1
-            --            end
-            --
-            ----            local nll_rewards = self.userScorePred:forward(prepUserState)
-            ----            lp, rin = torch.max(nll_rewards[1]:squeeze(), 1)
-            ----            if userAct == CIUserSimulator.CIFr.usrActInd_end and rin[1] == userRew then crcRewCnt = crcRewCnt + 1 end
-            --
-            --            tltCnt = tltCnt + 1
-            --
-            --            if userAct == CIUserSimulator.CIFr.usrActInd_end then
-            --                self.userActsPred:forget()
-            ----                self.userScorePred:forget()
-            --            end
-            --
-            --        end
-            --
-            --        print('###', crcActCnt/tltCnt, crcRewCnt/402, earlyCrcAct/earlyTotAct)
-            --
+            -- sharedLayer == 0 and not lstm models
+            for i=1, #CIUserSimulator.realUserDataStates do
+                local userState = CIUserSimulator:preprocessUserStateData(CIUserSimulator.realUserDataStates[i], opt.prepro)
+                local userAct = CIUserSimulator.realUserDataActs[i]
+                local userRew = CIUserSimulator.realUserDataRewards[i]
+
+                local prepUserState = torch.Tensor(1, CIUserSimulator.userStateFeatureCnt)
+                prepUserState[1] = userState:clone()
+
+                local nll_acts = self.userActsPred:forward(prepUserState)
+                lp, ain = torch.max(nll_acts[1]:squeeze(), 1)
+
+                if i == CIUserSimulator.realUserDataStartLines[userInd]+countScope then firstActDist[ain[1]] = firstActDist[ain[1]]+1 end   -- check act dist at each x-th time step
+                --            if ain[1] == userAct[opt.lstmHist] then crcActCnt = crcActCnt + 1 end
+                lpy, lps = torch.sort(nll_acts[1]:squeeze(), 1, true)
+                local crtExt = false
+                local smpLen = self.opt.actEvaScp    -- This is the range of selected actions to check for correct prediction
+                for l=1, smpLen do
+                    if lps[l] == userAct then
+                        crcActCnt = crcActCnt + 1
+                        crtExt = true
+                        break
+                    end
+                end
+
+                local indDiff = i - CIUserSimulator.realUserDataStartLines[userInd]
+                if indDiff >= 0 and indDiff <= opt.lstmHist+80 and i <= CIUserSimulator.realUserDataEndLines[userInd] then
+                    if crtExt then
+                        earlyCrcAct[indDiff+1] = earlyCrcAct[indDiff+1] + 1 -- This is at the indDiff's time step, what is prob of correct act prediction
+                    end
+                    earlyTotAct[indDiff+1] = earlyTotAct[indDiff+1] + 1
+                end
+                if i == CIUserSimulator.realUserDataEndLines[userInd] then
+                    userInd = userInd+1
+                end
+
+                local nll_rewards = self.userScorePred:forward(prepUserState)
+                lp, rin = torch.max(nll_rewards[1]:squeeze(), 1)
+                if userAct == CIUserSimulator.CIFr.usrActInd_end and rin[1] == userRew then crcRewCnt = crcRewCnt + 1 end
+
+                tltCnt = tltCnt + 1
+
+            end
+
+--            print('1st act: ', firstActDist)
+            print('Following stats are act pred accuracy, reward pred accuracy, act pred accu of each time step')
+            print('###', crcActCnt/tltCnt, crcRewCnt/#CIUserSimulator.realUserDataEndLines, torch.cdiv(earlyCrcAct, earlyTotAct))
         end
+
     else
+        -- opt.sharedLayer == 1
+        -- Unique Act/Score prediction model with shared lower layers
+        -- The user action/score predictors for evaluation should be pre-trained, and loaded from files
+        -- Also, the CIUserSimulator, CIUserActsPred, CIUserScorePred should be initialized using
+        -- the test set.
+        print('User Act Score shared-layer model: #', paths.concat(opt.ubgDir , opt.uapFile))
+        self.userActScorePred = torch.load(paths.concat(opt.ubgDir , opt.uapFile))
+        self.userActScorePred:evaluate()
+        self.CIUSim = CIUserSimulator
+        self.CIUasp = CIUserActScorePred
+        self.opt = opt
+
+        if opt.uppModel == 'lstm' then
+            -- sharedLayer == 1 and lstm model
+
+            --            self._actionDistributionCalc(CIUserSimulator, countScope)
+
+            self.userActScorePred:forget()
+
+            for i=1, #CIUserActScorePred.rnnRealUserDataStates do
+                local userState = CIUserActScorePred.rnnRealUserDataStates[i]
+                local userAct = CIUserActScorePred.rnnRealUserDataActs[i]
+                local userRew = CIUserActScorePred.rnnRealUserDataRewards[i]
+
+                local tabState = {}
+                for j=1, opt.lstmHist do
+                    local prepUserState = torch.Tensor(1, CIUserSimulator.userStateFeatureCnt)
+                    prepUserState[1] = CIUserSimulator:preprocessUserStateData(userState[j], opt.prepro)
+                    tabState[j] = prepUserState:clone()
+                end
+
+                local nll_acts = self.userActScorePred:forward(tabState)
+                lp, ain = torch.max(nll_acts[opt.lstmHist][1]:squeeze(), 1)     -- then 2nd [1] index is for action prediction from the shared act/score prediction outcome
+                if i == CIUserActScorePred.rnnRealUserDataStarts[userInd]+countScope then firstActDist[ain[1]] = firstActDist[ain[1]]+1 end   -- check act dist at each x-th time step
+                --            if ain[1] == userAct[opt.lstmHist] then crcActCnt = crcActCnt + 1 end
+                lpy, lps = torch.sort(nll_acts[opt.lstmHist][1]:squeeze(), 1, true)
+                local crtExt = false
+                local smpLen = self.opt.actEvaScp    -- This is the range of selected actions to check for correct prediction
+                for l=1, smpLen do
+                    if lps[l] == userAct[opt.lstmHist] then
+                        crcActCnt = crcActCnt + 1
+                        crtExt = true
+                        break
+                    end
+                end
+
+                local indDiff = i - CIUserActScorePred.rnnRealUserDataStarts[userInd]
+                if indDiff >= 0 and indDiff <= opt.lstmHist+80 and i <= CIUserActScorePred.rnnRealUserDataEnds[userInd] then
+                    if crtExt then
+                        earlyCrcAct[indDiff+1] = earlyCrcAct[indDiff+1] + 1 -- This is at the indDiff's time step, what is prob of correct act prediction
+                    end
+                    earlyTotAct[indDiff+1] = earlyTotAct[indDiff+1] + 1
+                end
+                if i == CIUserActScorePred.rnnRealUserDataEnds[userInd] then
+                    userInd = userInd+1
+                end
+
+                -- The predicted reward is the 2nd output of nll_acts in 2nd dim
+                lp, rin = torch.max(nll_acts[opt.lstmHist][2]:squeeze(), 1)
+                if userAct[opt.lstmHist] == CIUserSimulator.CIFr.usrActInd_end and rin[1] == userRew[opt.lstmHist] then crcRewCnt = crcRewCnt + 1 end
+
+                tltCnt = tltCnt + 1
+
+                self.userActScorePred:forget()
+
+            end
+
+            --            print('1st act: ', firstActDist)
+            print('Following stats are act pred accuracy, reward pred accuracy, act pred accu of each time step')
+            print('###', crcActCnt/tltCnt, crcRewCnt/#CIUserActScorePred.rnnRealUserDataEnds, torch.cdiv(earlyCrcAct, earlyTotAct))
+
+        else
+            -- SharedLayer == 1, and not lstm models
+            for i=1, #CIUserSimulator.realUserDataStates do
+                local userState = CIUserSimulator:preprocessUserStateData(CIUserSimulator.realUserDataStates[i], opt.prepro)
+                local userAct = CIUserSimulator.realUserDataActs[i]
+                local userRew = CIUserSimulator.realUserDataRewards[i]
+
+                local prepUserState = torch.Tensor(1, CIUserSimulator.userStateFeatureCnt)
+                prepUserState[1] = userState:clone()
+
+                local nll_acts = self.userActScorePred:forward(prepUserState)
+
+                -- Here, if moe is used with shared lower layers, it is the problem that,
+                -- due to limitation of MixtureTable module, we have to join tables together as
+                -- a single tensor as output of the whole user action and score prediction model.
+                -- So, to guarantee the compatability, we need split the tensor into two tables here,
+                -- for act prediction and score prediction respectively.
+                if opt.uppModel == 'moe' then
+                    nll_acts = nll_acts:split(CIUserSimulator.CIFr.usrActInd_end, 2)  -- We assume 1st dim is batch index. Act pred is the 1st set of output, having dim of 15. Score dim 2.
+                end
+
+                lp, ain = torch.max(nll_acts[1][1]:squeeze(), 1)
+
+                if i == CIUserSimulator.realUserDataStartLines[userInd]+countScope then firstActDist[ain[1]] = firstActDist[ain[1]]+1 end   -- check act dist at each x-th time step
+                --            if ain[1] == userAct[opt.lstmHist] then crcActCnt = crcActCnt + 1 end
+                lpy, lps = torch.sort(nll_acts[1][1]:squeeze(), 1, true)
+                local crtExt = false
+                local smpLen = self.opt.actEvaScp    -- This is the range of selected actions to check for correct prediction
+                for l=1, smpLen do
+                    if lps[l] == userAct then
+                        crcActCnt = crcActCnt + 1
+                        crtExt = true
+                        break
+                    end
+                end
+
+                local indDiff = i - CIUserSimulator.realUserDataStartLines[userInd]
+                if indDiff >= 0 and indDiff <= opt.lstmHist+80 and i <= CIUserSimulator.realUserDataEndLines[userInd] then
+                    if crtExt then
+                        earlyCrcAct[indDiff+1] = earlyCrcAct[indDiff+1] + 1 -- This is at the indDiff's time step, what is prob of correct act prediction
+                    end
+                    earlyTotAct[indDiff+1] = earlyTotAct[indDiff+1] + 1
+                end
+                if i == CIUserSimulator.realUserDataEndLines[userInd] then
+                    userInd = userInd+1
+                end
+
+                -- The predicted reward is the 2nd output of nll_acts in 2nd dim
+                lp, rin = torch.max(nll_acts[2][1]:squeeze(), 1)
+                if userAct == CIUserSimulator.CIFr.usrActInd_end and rin[1] == userRew then crcRewCnt = crcRewCnt + 1 end
+
+                tltCnt = tltCnt + 1
+
+            end
+
+            --            print('1st act: ', firstActDist)
+            print('Following stats are act pred accuracy, reward pred accuracy, act pred accu of each time step')
+            print('###', crcActCnt/tltCnt, crcRewCnt/#CIUserSimulator.realUserDataEndLines, torch.cdiv(earlyCrcAct, earlyTotAct))
+        end
 
     end
+end
+
+function CIUserBehaviorGenEvaluator:_actionDistributionCalc(CIUserSimulator, cntScope)
+    local countScope = cntScope
+    local st = torch.Tensor(CIUserSimulator.CIFr.usrActInd_end):fill(0)     -- tensor dim is 15 (user action types)
+    for k, v in ipairs(CIUserSimulator.realUserDataStartLines) do
+        st[CIUserSimulator.realUserDataActs[v+countScope]] = st[CIUserSimulator.realUserDataActs[v+countScope]] +1 -- check act dist at each x-th time step
+    end
+    print('Act count at time step ', countScope+1, ' is:', st)
 end
 
 return CIUserBehaviorGenEvaluator
