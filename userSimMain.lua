762f5ed4e6888cb5be7fda777c466cfa414b9129
Pengcheng Wang <pwang8@ncsu.edu>
2017-05-10 19:03:24 -0400

    Update training params in opt

--- userSimMain.lua
+++ userSimMain.lua
@@ -24,8 +24,8 @@ opt = lapp[[
        -t,--threads       (default 4)           number of threads
        -g,--gpu        (default 0)          gpu device id, 0 for using cpu
        --prepro           (default "std")       input state feature preprocessing: rsc | std
-       --lstmHd           (default 192)          lstm hidden layer size
-       --lstmHist         (default 6)           lstm hist length. This influence the rnn tensor table construction in data preparation
+       --lstmHd           (default 64)          lstm hidden layer size
+       --lstmHist         (default 10)           lstm hist length. This influence the rnn tensor table construction in data preparation
        --uSimLstmBackLen         (default 3)           The maximum step applied in btpp in lstm
        --ubgDir           (default "ubgModel")  directory storing uap and usp models
        --uapFile          (default "uap.t7")          file storing userActsPredictor model
