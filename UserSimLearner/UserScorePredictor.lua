a3c83303dc2994728268a9efddb5f112599b02da
Pengcheng Wang <pwang8@ncsu.edu>
2017-05-11 00:01:06 -0400

    Readjusted params.

--- UserSimLearner/UserScorePredictor.lua
+++ UserSimLearner/UserScorePredictor.lua
@@ -491,7 +491,7 @@ function CIUserScorePredictor:trainOneEpoch()
     print('<trainer> saving ciunet to '..filename)
     torch.save(filename, self.model)
 
-    if self.trainEpoch % 5 == 0 then
+    if self.trainEpoch % 10 == 0 then
         filename = paths.concat(self.opt.save, string.format('%d', self.trainEpoch)..'_'..string.format('%.2f', self.uspConfusion.totalValid*100)..'usp.t7')
         os.execute('mkdir -p ' .. sys.dirname(filename))
         print('<trainer> saving periodly trained ciunet to '..filename)
